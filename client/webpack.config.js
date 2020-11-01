9f497ee839e46025d0ab5bf164c3764ad1bddc89
MyeongSeong <maeng2418@naver.com>
2020-11-02 01:34:13 +0900

    [chore] webpack 설정

---
+++ client/webpack.config.js
@@ -0,0 +1,59 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: 'index.js',
+    sourceMapFilename: 'index.js.map',
+  },
+  resolve: {
+    alias: {
+      components: path.resolve(__dirname, 'client/src/components'),
+    },
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: path.join(__dirname, '/public'),
+    port: 9000,
+    proxy: {
+      '/api/': {
+        target: 'http://127.0.0.1:3000',
+        changeOrigin: true,
+      },
+    },
+    historyApiFallback: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: ['file-loader'],
+      },
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              [
+                '@babel/preset-env',
+                {
+                  corejs: 3,
+                  targets: '> 1%',
+                  useBuiltIns: 'usage',
+                },
+              ],
+            ],
+            plugins: ['@babel/plugin-proposal-class-properties'],
+          },
+        },
+      },
+    ],
+  },
+};
