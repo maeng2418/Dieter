33f7b9051ed1fe63e28aa4476718990b6ad9cd41
maeng2418 <maeng2418@naver.com>
2020-11-04 03:18:07 +0900

    [chore] 이벤트 핸들러 하나의 디렉토리에서 관리

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -1,8 +1,8 @@
 import { Layout } from 'components';
 import MainPage from './main';
-import onEventHandler from '../event-controller';
+import onEventHandler from '../event-handler';
 import { setState, setEvent } from '../store';
-import { onStoreDateHandler } from '../store/event-handler';
+import { onStoreDateHandler } from '../event-handler/store-event';
 
 const App = () => {
   setState('date', new Date());
